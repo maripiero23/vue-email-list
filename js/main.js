b383815518aefc3d3fed21da308172b52a683879
maripiero23 <mari.pieropan@gmail.com>
2022-11-30 17:23:09 +0100

    1 dei pochissimi bonus fatti, finche si puo godiamocel

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const { createApp } = Vue;
+
+const app = createApp({
+  data () {
+    return {
+      listaEmails: [],
+      listaProvvisoria: []
+
+    };
+  },
+  methods: {
+
+        // recupero la lista mail tramite api
+        addEmail(){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((resp) => {
+              this.listaProvvisoria.push(resp.data.response)
+              this.counter++
+              console.log(this.counter);
+              if(this.counter === 10){
+                this.listaEmails = this.listaProvvisoria
+              }
+
+    
+            });
+        } 
+
+
+    },
+    
+  
+  mounted () {
+    this.counter = 0
+    for (let i = 1; i <= 10; i++) {
+        this.addEmail();
+    
+  }
+}
+}).mount('#app');
